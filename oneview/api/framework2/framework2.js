a2a7d376867db179996ab89d064ac0eaec400d28
Madhu Chakravarthy <ncmadhu@gmail.com>
2017-03-28 17:39:17 +0530

    Adding xml parsing code

--- oneview/api/framework2/framework2.js
+++ oneview/api/framework2/framework2.js
@@ -10,12 +10,18 @@ var router = express.Router()
 var config = require('config')
 var logger = require('../log').logger
 var fs =  require('fs')
+var parseString = require('xml2js').parseString;
 
 var reportParser = function(req, res) {
-    var filePath = process.cwd() + '/reports/framework2/report.xml' 
+    //var filePath = process.cwd() + '/reports/framework2/report.xml' 
+    var filePath = process.cwd() + '/reports/framework2/output.xml' 
     logger.debug("filePath:" + filePath)
     fs.readFile(filePath, 'utf8', function(err, contents) {
-        res.end(contents)
+        parseString(contents, function(err, result) {
+            response = JSON.stringify(result)
+            console.log(JSON.stringify(result))
+            res.end(response)
+        })
     })
 }
 
