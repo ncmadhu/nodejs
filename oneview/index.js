a531f268162dba156740c762f350e09035919f46
Madhu Chakravarthy <madhuc@altencalsoftlabs.com>
2017-04-16 00:02:38 +0530

    Added code to trigger test from ui

--- oneview/index.js
+++ oneview/index.js
@@ -7,6 +7,8 @@ Author: Madhu Chakravarthy
 var express = require('express')
 var config = require('config')
 var logger = require('./api/common/log').logger
+var mongoDb = require('./api/common/mongoClient')
+var rabbitMQ = require('./api/common/rabbitMQ')
 
 var app = express()
 const port = config.get('server.port')
@@ -46,9 +48,30 @@ app.use(errHandler)
 Routes
 */
 
-//Response for get request
+//Response for main page 
 app.get('/', function (req, res) {
-    res.send('Request Received')
+    res.redirect('/dashboard');
+})
+
+
+//Response for dashboard get request
+app.get('/dashboard', function (req, res) {
+    mongoDb.getDashboardData(req, res)
+})
+
+//Response for suite get request
+app.get('/suites', function (req, res) {
+    mongoDb.getSuitesData(req, res)
+})
+
+//Response for test get request
+app.get('/tests', function (req, res) {
+    mongoDb.getTestsData(req, res)
+})
+
+//Response for test start request
+app.get('/test/start', function (req, res) {
+    rabbitMQ.startTest(req, res)
 })
 
 //unsupported URL
@@ -60,6 +83,7 @@ app.use(function (req, res, next) {
 //Start the server
 
 app.listen(port, function () {
+    mongoDb.createIndex('results')
     console.log('Listening on port: ' + port)
 })
 
